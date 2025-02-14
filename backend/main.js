77a68af811d6d94b35a84383b1a2208bbce44627
Sor171024 <piwat.4389@gmail.com>
2025-02-14 16:40:55 +0700

    .

--- backend/main.js
+++ backend/main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { readdirSync } = require("fs");
-const PORT = 8080;
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
